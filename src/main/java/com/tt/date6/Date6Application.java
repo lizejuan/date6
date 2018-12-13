908f256eea3142dc7e8cc49d4a2082d84156ab79
lizejuan <13313571683@163.com>
2018-12-13 10:48:30 +0800

    第一次提交该项目

---
+++ src/main/java/com/tt/date6/Date6Application.java
@@ -0,0 +1,14 @@
+package com.tt.date6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Date6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Date6Application.class, args);
+	}
+
+}
+
